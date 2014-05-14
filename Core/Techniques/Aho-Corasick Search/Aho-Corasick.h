daa17a8f61352e0bad0a231856a1a4a2a3460962
Mohammedbie <mohammedbie@hotmail.com>
2014-05-14 22:08:16 +0200

    Update Aho-Corasick.h
    Changing CreateTrie parameter to void

--- Core/Techniques/Aho-Corasick Search/Aho-Corasick.h
+++ Core/Techniques/Aho-Corasick Search/Aho-Corasick.h
@@ -12,7 +12,7 @@ class Techniques::Static::AhoCorasick
    public:
    AhoCorasick(void);
    AhoCorasick(char* DB_Path,char* text,unsigned int text_size);
-   void CreateTrie(char* DB_Path);
+   void CreateTrie(void);
    int Search(char* text,unsigned int text_size);
 };
 
